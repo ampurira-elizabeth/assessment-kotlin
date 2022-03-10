6ccdf724d9640c182f8b09f23ddea71ec033afbd
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-03-10 08:43:54 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,35 @@
+fun main(){
+var name ="codeHive"
+    var char1=[5]
+    var char2=6
+    var char3=7
+    var char4=8
+    println(char1)
+
+
+
+
+   var y =two(100,40)
+    var number1=100
+    var number2=40
+    var x=number1%number2
+    println(x)
+    three("elizabeth",60)
+    four()
+}
+fun two(number1:Int,number2:Int):Int{
+    var modulus=number1%number2
+    return modulus
+}
+fun three(name:String,age:Int){
+    var name="Elizabeth"
+    var age= 60
+    var sentence = "hy"+" " +"my name is"+" "+ name + " "+"and i am"+" " +age+ "years"+" "+ "old"
+    println(sentence)
+
+}
+fun four(){
+    var name="women"
+    var x =name.length
+    println(x)
+}

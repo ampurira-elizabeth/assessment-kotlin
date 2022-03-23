233fe4d47f90d236d9651aa1b2a3661272fe3036
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-03-23 08:37:22 +0300

    carol

--- src/main.kt
+++ src/main.kt
@@ -3,8 +3,8 @@ var name ="codeHive"
     println(charArrayOf('5','6','7','8'))
 
    var y =two(100,40)
-    var number1=100
-    var number2=40
+    var number1=300
+    var number2=90
     var x=number1%number2
     println(x)
     three("elizabeth",60)
@@ -15,8 +15,8 @@ fun two(number1:Int,number2:Int):Int{
     return modulus
 }
 fun three(name:String,age:Int){
-    var name="Elizabeth"
-    var age= 60
+    var name="Eliz carol"
+    var age= 70
     var sentence = "hy"+" " +"my name is"+" "+ name + " "+"and i am"+" " +age+ "years"+" "+ "old"
     println(sentence)
 
